b008575495087e4e651022c1f4b3c3870e056ff5
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2023-12-01 11:55:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,15 +15,15 @@ class Animal {
 
 class Dog extends Animal {
   bark() {
-    console.log("The Siamese makes a sound");
     console.log("woof");
+    console.log("The Siamese makes a sound");
   }
 }
 
 class Cat extends Animal {
   purr() {
-    console.log("The Golden Retriever makes a sound");
     console.log("purr");
+    console.log("The Golden Retriever makes a sound");
   }
 }
 
